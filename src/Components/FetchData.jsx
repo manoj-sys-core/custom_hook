eeb01fcea0bcdce271fd5772923d434631ef4a43
Manoj <manojcs6317@gmail.com>
2025-10-03 18:49:46 +0530

    initial commit

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import UseFetch from "./UseFetch";
 
 const FetchData = () => {
+  const [data] = UseFetch(
+    "https://api.npoint.io/9045c260b1565daa9e15"
+  );
+  const [poses]  = UseFetch("https://api.npoint.io/4459a9a10e43812e1152")
   return (
-    <>
-     <ul className='list_data_main'>
-        <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
-     </ul>
-    </>
-  )
-}
+    <div className="list_data_main">
+      <h1 className="usefetch_heading">Use Fetch Custom Hook</h1>
+      <ul>
+        {data &&
+          data.map((item, index) => (
+            <li key={index}>
+              <h1>{item?.name}</h1>
+              <img src={item.image} alt="data" width="200" />
+              <h2>{item?.benifits}</h2>
+              <h3>{item?.importance}</h3>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
-export default FetchData
+export default FetchData;
--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
 const UseFetch = (url) => {
-    
+    const [data,setdata] = useState([])
+    const [load,setload] = useState(false)
+    useEffect(()=>{
+        try{
+        setload(true)
+        axios.get(url)
+        .then(Response => setdata(Response.data))
+        }
+        catch(err){
+            console.log(err)
+        }
+        finally{
+            setload(false)
+        }
+    },[url])
+    return [data,load]
 }
 
 export default UseFetch
